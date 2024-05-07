9640ab86ea95709cfcdc35e11cb0f07b2f6035a9
Hzl0929 <80333221+Hzl0929@users.noreply.github.com>
2024-05-07 19:00:47 +0800

    Create The Gaussian Elimination.py

---
+++ The Gaussian Elimination.py
@@ -0,0 +1,29 @@
+#Bringing it all together
+
+def gaussian_elimination(A, B):
+    """
+    Solve a linear system represented by an augmented matrix using the Gaussian elimination method.
+
+    Parameters:
+    - A (numpy.array): Square matrix of size n x n representing the coefficients of the linear system
+    - B (numpy.array): Column matrix of size 1 x n representing the constant terms.
+
+    Returns:
+    numpy.array or str: The solution vector if a unique solution exists, or a string indicating the type of solution.
+    """
+
+    ### START CODE HERE ###
+
+    # Get the matrix in row echelon form
+    row_echelon_M = row_echelon_form(A, B)
+
+    # If the system is non-singular, then perform back substitution to get the result. 
+    # Since the function row_echelon_form returns a string if there is no solution, let's check for that.
+    # The function isinstance checks if the first argument has the type as the second argument, returning True if it does and False otherwise.
+    if not isinstance(row_echelon_M, str): 
+        solution = back_substitution(row_echelon_M)
+
+    ### END SOLUTION HERE ###
+
+    return solution
+        
